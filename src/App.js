53bd56084c3d6c6811810bec1541f7b8a8f68b3c
ljh666 <13216698987@163.com>
2019-04-29 21:52:30 +0800

    v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState, useCallback} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import Home from './containers/Home'
+import React, { useState, useCallback } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./containers/Home";
+import Create from './containers/Create'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/create">Create</Link>
+          <Link to="/edit/10">Edit</Link>
+        </ul>
+        <div className="container pb-5">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/create" component={Create} />
+          <Route exact path="/edit/:id" component={Create} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/Create.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Create = ({match}) => {
+  return <h1>this is create {match.params.id} page</h1>
+}
+
+export default Create
